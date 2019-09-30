14c8ed528582aecb6a5cac1b1ca418eacf51c432
Teoman Soygul <teo@soygul.com>
2019-09-30 14:43:56 +0200

    subarry: add negative number test case

--- algorithms/arrays/sub-arrays-with-given-sum.js
+++ algorithms/arrays/sub-arrays-with-given-sum.js
@@ -2,9 +2,10 @@ const assert = require('assert')
 
 /**
  * Given a list of integers, finds the contiguous sub-lists of integers adds up to a number.
- * @param inputArr
- * @param desiredSum
- * @returns {[]}
+ *
+ * @param inputArr - An array of integers (0, negative, or positive numbers).
+ * @param desiredSum - An integer.
+ * @returns {[]} - An array of subarrays that add up to the desired sum.
  */
 function subArrays (inputArr, desiredSum) {
   let sum = 0
@@ -38,4 +39,15 @@ const calculatedSolution1 = subArrays(exampleInput1, desiredSum1)
 
 assert.deepStrictEqual(calculatedSolution1, actualSolution1)
 
-console.log(`Example Input: ${JSON.stringify(exampleInput1)}, Desired Sum: ${desiredSum1}, Solution: ${JSON.stringify(calculatedSolution1)}`)
+console.log(`Example Input #1: ${JSON.stringify(exampleInput1)}, Desired Sum: ${desiredSum1}, Solution: ${JSON.stringify(calculatedSolution1)}`)
+
+// test case #2
+const exampleInput2 = [10, 2, -2, -20, 10, -3, -7]
+const desiredSum2 = -10
+const actualSolution2 = [[10, 2, -2, -20], [-3, -7]]
+
+const calculatedSolution2 = subArrays(exampleInput2, desiredSum2)
+
+assert.deepStrictEqual(calculatedSolution2, actualSolution2)
+
+console.log(`Example Input: ${JSON.stringify(exampleInput2)}, Desired Sum: ${desiredSum2}, Solution: ${JSON.stringify(calculatedSolution2)}`)
