1948af0069ba26f14f4773248569f3eb792e670d
Teoman Soygul <teo@soygul.com>
2020-01-27 23:16:30 +0300

    sliding-window: fix iterator

--- algorithms/dynamic-programming/sliding-window/shortest-substring-of-letters.js
+++ algorithms/dynamic-programming/sliding-window/shortest-substring-of-letters.js
@@ -1,10 +1,32 @@
 const assert = require('assert')
 
 /**
- *
- * @param string
- * @param letters
+ * Given a string and n characters, find the shortest substring that contains all given characters.
+ * @param string - Any string.
+ * @param characters - An array of characters that the substring should contain.
+ * @returns {string} - The shortest substring containing all the desired characters.
  */
-function getShortestSubstring(string, letters) {
+function getShortestSubstring(string, characters) {
+  let substringStartIndex = 0
 
+  for (let i = 0; i < string.length; i++) {
+
+  }
+
+  return 'asdf'
 }
+
+
+/**
+ * Tests
+ */
+
+// test case #1
+const exampleInput1 = 'asb1.9d/d"!304#b$%^%!ksd,1294iubasdmc'
+const desiredCharacters1 = ['b', '$', '#', '1']
+const solution1 = '#b$%^%!ksd,1'
+
+const calculatedSolution1 = getShortestSubstring(exampleInput1, desiredCharacters1)
+
+console.log(`Example Input #1: ${exampleInput1}, Desired Characters: ${desiredCharacters1}, Solution: ${calculatedSolution1}`)
+assert.deepStrictEqual(calculatedSolution1, solution1)
