f0c2f2fa5db94596ea6c4f64e8e1533d0c00f4ce
Teoman Soygul <teo@soygul.com>
2020-01-29 20:15:59 +0300

    sliding-window: fix test order

--- algorithms/dynamic-programming/sliding-window/shortest-substring-of-letters.js
+++ algorithms/dynamic-programming/sliding-window/shortest-substring-of-letters.js
@@ -3,30 +3,46 @@ const assert = require('assert')
 /**
  * Given a string and n characters, find the shortest substring that contains all given characters.
  * @param string - Any string.
- * @param characters - An array of characters that the substring should contain.
+ * @param characters - A string of characters that the substring should contain.
  * @returns {string} - The shortest substring containing all the desired characters.
  */
-function getShortestSubstring(string, characters) {
-  let substringStartIndex = 0
+function getShortestSubstring (string, characters) {
+  const substringStartIndex = 0; let substringEndIndex = 0
+  let neededChars = characters.split('')
 
   for (let i = 0; i < string.length; i++) {
+    let j = neededChars.indexOf(string[i])
+    if (j > -1) neededChars.splice(j, 1)
 
+    if (neededChars.length === 0) {
+      substringEndIndex = i
+      break
+    }
   }
 
-  return 'asdf'
+  return string.slice(substringStartIndex, substringEndIndex + 1)
 }
-
 
 /**
  * Tests
  */
 
 // test case #1
-const exampleInput1 = 'asb1.9d/d"!304#b$%^%!ksd,1294iubasdmc'
-const desiredCharacters1 = ['b', '$', '#', '1']
-const solution1 = '#b$%^%!ksd,1'
+const exampleInput1 = 'a93kdabc991cba35fg'
+const desiredCharacters1 = 'abcabc'
+const solution1 = 'abc991cba'
 
 const calculatedSolution1 = getShortestSubstring(exampleInput1, desiredCharacters1)
 
 console.log(`Example Input #1: ${exampleInput1}, Desired Characters: ${desiredCharacters1}, Solution: ${calculatedSolution1}`)
 assert.deepStrictEqual(calculatedSolution1, solution1)
+
+// test case #2
+const exampleInput2 = 'asb2.9d/d"!304#b$%^%!ksd,2294iubasdmc'
+const desiredCharacters2 = 'b$#2'
+const solution2 = '#b$%^%!ksd,2'
+
+const calculatedSolution2 = getShortestSubstring(exampleInput2, desiredCharacters2)
+
+console.log(`Example Input #2: ${exampleInput2}, Desired Characters: ${desiredCharacters2}, Solution: ${calculatedSolution2}`)
+assert.deepStrictEqual(calculatedSolution2, solution2)
