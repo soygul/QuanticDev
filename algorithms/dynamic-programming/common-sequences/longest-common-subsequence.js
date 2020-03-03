75251b94a9f1142dc39295e3d509d25c105af0a5
Teoman Soygul <teo@soygul.com>
2020-03-03 16:19:05 +0100

    common-sequence: add lcs template

---
+++ algorithms/dynamic-programming/common-sequences/longest-common-subsequence.js
@@ -0,0 +1,35 @@
+const assert = require('assert')
+
+/**
+ * Given two arrays (or strings), find their longest common subsequence.
+ *
+ * @param inputArr1 - First array/string.
+ * @param inputArr2 - Second array/string.
+ * @returns {[]} - Longest common subsequence of the two inputs.
+ */
+function longestCommonSubsequence (inputArr1, inputArr2) {
+  let lcs = []
+
+  inputArr1.forEach((char1, index1) => {
+    inputArr2.forEach((char2, index2) => {
+      // rules:
+      //   if char1 !== char2: take largest of the right diagonal value
+      //   if char1 === char2: take left diagonal value + 1
+      if (char1 === char2) lcs.push([])
+    })
+  })
+}
+
+/**
+ * Tests
+ */
+
+// test case #1
+const inputArr1 = 'xabcf'
+const inputArr2 = 'ayfo'
+const solution1 = 'af'
+
+const calculatedSolution1 = longestCommonSubsequence(inputArr1, inputArr2)
+
+console.log(`Example Input #1: ${inputArr1}, Input #2: ${inputArr2}, Solution: ${calculatedSolution1}`)
+assert.deepStrictEqual(calculatedSolution1, solution1)
