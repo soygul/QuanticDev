9d3716d7573df4d4153025fe2e2e55d3507c1cd6
Teoman Soygul <teo@soygul.com>
2020-06-06 10:19:22 +0200

    kadanes: add more test cases

--- algorithms/dynamic-programming/kadanes-algorithm/max-min-sum-subarray.js
+++ algorithms/dynamic-programming/kadanes-algorithm/max-min-sum-subarray.js
@@ -10,7 +10,7 @@ const assert = require('assert')
  */
 function getMaxMinSubarray (inputArr, isMaxSum = true) {
   // validate the input
-  assert(inputArr.length > 0, 'Input array should have at least one element.')
+  assert(Array.isArray(inputArr) && inputArr.every(n => Number.isInteger(n)), 'Input should be an array of integers.')
 
   // now calculate the maximum (or minimum) sum up to each index using kadane's algo
   let currentSum = 0; let currentSumStart = 0; let currentSumEnd = 0
@@ -48,4 +48,50 @@ console.log(`Example Input #1: ${JSON.stringify(exampleInput1)}, Max Sum Subarra
 assert.deepStrictEqual(calculatedMaxSolution1, maxSolution1)
 assert.deepStrictEqual(calculatedMinSolution1, minSolution1)
 
-// all negative numbers, all 0, and other edge cases
+// test case #2
+const exampleInput2 = []
+const maxSolution2 = []
+const minSolution2 = []
+
+const calculatedMaxSolution2 = getMaxMinSubarray(exampleInput2, true)
+const calculatedMinSolution2 = getMaxMinSubarray(exampleInput2, false)
+
+console.log(`Example Input #2: ${JSON.stringify(exampleInput2)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution2)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution2)}`)
+assert.deepStrictEqual(calculatedMaxSolution2, maxSolution2)
+assert.deepStrictEqual(calculatedMinSolution2, minSolution2)
+
+// test case #3
+const exampleInput3 = [0, 0, 0]
+const maxSolution3 = [0]
+const minSolution3 = [0]
+
+const calculatedMaxSolution3 = getMaxMinSubarray(exampleInput3, true)
+const calculatedMinSolution3 = getMaxMinSubarray(exampleInput3, false)
+
+console.log(`Example Input #3: ${JSON.stringify(exampleInput3)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution3)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution3)}`)
+assert.deepStrictEqual(calculatedMaxSolution3, maxSolution3)
+assert.deepStrictEqual(calculatedMinSolution3, minSolution3)
+
+// test case #4
+const exampleInput4 = [-1, -2, -3, -4, -5]
+const maxSolution4 = []
+const minSolution4 = [-15]
+
+const calculatedMaxSolution4 = getMaxMinSubarray(exampleInput4, true)
+const calculatedMinSolution4 = getMaxMinSubarray(exampleInput4, false)
+
+console.log(`Example Input #4: ${JSON.stringify(exampleInput4)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution4)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution4)}`)
+assert.deepStrictEqual(calculatedMaxSolution4, maxSolution4)
+assert.deepStrictEqual(calculatedMinSolution4, minSolution4)
+
+// test case #5
+const exampleInput5 = [1, 2, 3, 4, 5]
+const maxSolution5 = [1, 2, 3, 4, 5]
+const minSolution5 = []
+
+const calculatedMaxSolution5 = getMaxMinSubarray(exampleInput5, true)
+const calculatedMinSolution5 = getMaxMinSubarray(exampleInput5, false)
+
+console.log(`Example Input #5: ${JSON.stringify(exampleInput5)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution5)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution5)}`)
+assert.deepStrictEqual(calculatedMaxSolution5, maxSolution5)
+assert.deepStrictEqual(calculatedMinSolution5, minSolution5)
