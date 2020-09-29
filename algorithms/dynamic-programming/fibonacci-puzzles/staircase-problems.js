2bc750590610d98145e3ca3a24dd128ea1b05075
Teoman Soygul <teo@soygul.com>
2020-09-29 12:32:46 +0200

    fib: staircase: fix test ordering

--- algorithms/dynamic-programming/fibonacci-puzzles/staircase-problems.js
+++ algorithms/dynamic-programming/fibonacci-puzzles/staircase-problems.js
@@ -14,7 +14,15 @@ const assert = require('assert')
 //   Time complexity: O()
 //   Space complexity: O()
 //   Call Stack: O()
-function climbStairsRecursive (stairCount, maxSteps) {}
+function climbStairsRecursive (stairCount, maxSteps) {
+  let stepsTaken = 0
+
+  for (let i = 1; i <= maxSteps && stairCount - 1 > 0; i++) {
+    stepsTaken += climbStairsRecursive(stairCount - 1, maxSteps)
+  }
+
+  return stepsTaken
+}
 
 // acceptable solution with:
 //   Time complexity: O()
@@ -29,13 +37,13 @@ function climbStairsRecursiveMemoized (stairCount, maxSteps) {}
 function climbStairsFibonacci (stairCount, maxSteps) {
   // validate input
   assert(stairCount >= 0, 'Cannot have negative stairs.')
-  assert(m > 0, 'Max no of stairs you can climb at a time must be greater than 0.')
+  assert(maxSteps > 0, 'Max no of stairs you can climb at a time must be greater than 0.')
 
   // first two fibonacci-like numbers are always 0 and 1 so just return them as is
   if (stairCount <= 1) return stairCount
 
   // if you can only take 1 step at a time, no matter how high the stairs, there is only 1 way to climb it
-  if (m === 1) return 1
+  if (maxSteps === 1) return 1
 
   // fibonacci-like numbers sequence where each number is the sum of m numbers before it
   // first 2 fibonacci numbers are 0 and 1 so we start with those
@@ -47,20 +55,20 @@ function climbStairsFibonacci (stairCount, maxSteps) {
     fib.push(fib.reduce((total, n) => total + n))
 
     // we only need to know last m numbers so remove any extras
-    if (fib.length > m) fib.shift()
+    if (fib.length > maxSteps) fib.shift()
   }
 
   return fib.pop()
 }
 
 /**
- * very similar climbStairs except possibleSteps = [1, 2, 4, 7]
+ * very similar climbStairs except possibleStepsList = [1, 2, 4, 7]
  * this can still be solved using fibonacci-like sequences instead of recursion,
  * but we need to calculate all of [1, 2, 3, 4, 5, 6, 7] since upper steps can depend on any of them
  * @param stairCount
- * @param possibleSteps
+ * @param possibleStepsList
  */
-function climbStairsWithVariableSteps (stairCount, possibleSteps) {}
+function climbStairsWithVariableSteps (stairCount, possibleStepsList) {}
 
 /**
  * Tests
@@ -71,95 +79,95 @@ const stairs0 = 0
 const maxSteps0 = 2
 const solution0 = 0
 
-const calculatedSolution0 = climbStairsFibonacci(stairs0, maxSteps0)
 const calculatedSolution0_recursive = climbStairsRecursive(stairs0, maxSteps0)
 const calculatedSolution0_recursiveMemoized = climbStairsRecursiveMemoized(stairs0, maxSteps0)
+const calculatedSolution0_fibonacci = climbStairsFibonacci(stairs0, maxSteps0)
 
-console.log(`Example Stairs #0: ${stairs0}, maxSteps: ${m0}, Solution: ${solution0}`)
-assert.deepStrictEqual(calculatedSolution0, solution0)
+console.log(`Example Stairs #0: ${stairs0}, maxSteps: ${maxSteps0}, Solution: ${solution0}`)
 assert.deepStrictEqual(calculatedSolution0_recursive, solution0)
 assert.deepStrictEqual(calculatedSolution0_recursiveMemoized, solution0)
+assert.deepStrictEqual(calculatedSolution0_fibonacci, solution0)
 
 // test case #1
 const stairs1 = 1
 const maxSteps1 = 2
 const solution1 = 1
 
-const calculatedSolution1 = climbStairsFibonacci(stairs1, maxSteps1)
 const calculatedSolution1_recursive = climbStairsRecursive(stairs1, maxSteps1)
 const calculatedSolution1_recursiveMemoized = climbStairsRecursiveMemoized(stairs1, maxSteps1)
+const calculatedSolution1_fibonacci = climbStairsFibonacci(stairs1, maxSteps1)
 
-console.log(`Example Stairs #1: ${stairs1}, maxSteps: ${m1}, Solution: ${solution1}`)
-assert.deepStrictEqual(calculatedSolution1, solution1)
+console.log(`Example Stairs #1: ${stairs1}, maxSteps: ${maxSteps1}, Solution: ${solution1}`)
 assert.deepStrictEqual(calculatedSolution1_recursive, solution1)
 assert.deepStrictEqual(calculatedSolution1_recursiveMemoized, solution1)
+assert.deepStrictEqual(calculatedSolution1_fibonacci, solution1)
 
 // test case #2
 const stairs2 = 2
 const maxSteps2 = 2
 const solution2 = 2
 
-const calculatedSolution2 = climbStairsFibonacci(stairs2, maxSteps2)
 const calculatedSolution2_recursive = climbStairsRecursive(stairs2, maxSteps2)
 const calculatedSolution2_recursiveMemoized = climbStairsRecursiveMemoized(stairs2, maxSteps2)
+const calculatedSolution2_fibonacci = climbStairsFibonacci(stairs2, maxSteps2)
 
-console.log(`Example Stairs #2: ${stairs2}, maxSteps: ${m2}, Solution: ${solution2}`)
-assert.deepStrictEqual(calculatedSolution2, solution2)
+console.log(`Example Stairs #2: ${stairs2}, maxSteps: ${maxSteps2}, Solution: ${solution2}`)
 assert.deepStrictEqual(calculatedSolution2_recursive, solution2)
 assert.deepStrictEqual(calculatedSolution2_recursiveMemoized, solution2)
+assert.deepStrictEqual(calculatedSolution2_fibonacci, solution2)
 
 // test case #3
 const stairs3 = 7
 const maxSteps3 = 2
 const solution3 = 21
 
-const calculatedSolution3 = climbStairsFibonacci(stairs3, maxSteps3)
 const calculatedSolution3_recursive = climbStairsRecursive(stairs3, maxSteps3)
 const calculatedSolution3_recursiveMemoized = climbStairsRecursiveMemoized(stairs3, maxSteps3)
+const calculatedSolution3_fibonacci = climbStairsFibonacci(stairs3, maxSteps3)
 
-console.log(`Example Stairs #3: ${stairs3}, maxSteps: ${m3}, Solution: ${solution3}`)
-assert.deepStrictEqual(calculatedSolution3, solution3)
+console.log(`Example Stairs #3: ${stairs3}, maxSteps: ${maxSteps3}, Solution: ${solution3}`)
 assert.deepStrictEqual(calculatedSolution3_recursive, solution3)
 assert.deepStrictEqual(calculatedSolution3_recursiveMemoized, solution3)
+assert.deepStrictEqual(calculatedSolution3_fibonacci, solution3)
 
 // test case #4
 const stairs4 = 40
 const maxSteps4 = 1
 const solution4 = 1
 
-const calculatedSolution4 = climbStairsFibonacci(stairs4, maxSteps4)
 const calculatedSolution4_recursive = climbStairsRecursive(stairs4, maxSteps4)
 const calculatedSolution4_recursiveMemoized = climbStairsRecursiveMemoized(stairs4, maxSteps4)
+const calculatedSolution4_fibonacci = climbStairsFibonacci(stairs4, maxSteps4)
 
-console.log(`Example Stairs #4: ${stairs4}, maxSteps: ${m4}, Solution: ${solution4}`)
-assert.deepStrictEqual(calculatedSolution4, solution4)
+console.log(`Example Stairs #4: ${stairs4}, maxSteps: ${maxSteps4}, Solution: ${solution4}`)
 assert.deepStrictEqual(calculatedSolution4_recursive, solution4)
 assert.deepStrictEqual(calculatedSolution4_recursiveMemoized, solution4)
+assert.deepStrictEqual(calculatedSolution4_fibonacci, solution4)
 
 // test case #5
 const stairs5 = 16
 const maxSteps5 = 7
 const solution5 = 31489
 
-const calculatedSolution5 = climbStairsFibonacci(stairs5, maxSteps5)
 const calculatedSolution5_recursive = climbStairsRecursive(stairs5, maxSteps5)
 const calculatedSolution5_recursiveMemoized = climbStairsRecursiveMemoized(stairs5, maxSteps5)
+const calculatedSolution5_fibonacci = climbStairsFibonacci(stairs5, maxSteps5)
 
-console.log(`Example Stairs #5: ${stairs5}, maxSteps: ${m5}, Solution: ${solution5}`)
-assert.deepStrictEqual(calculatedSolution5, solution5)
+console.log(`Example Stairs #5: ${stairs5}, maxSteps: ${maxSteps5}, Solution: ${solution5}`)
 assert.deepStrictEqual(calculatedSolution5_recursive, solution5)
 assert.deepStrictEqual(calculatedSolution5_recursiveMemoized, solution5)
+assert.deepStrictEqual(calculatedSolution5_fibonacci, solution5)
 
 // test case #6
 const stairs6 = 30
 const maxSteps6 = 30
 const solution6 = 536870912
 
-const calculatedSolution6 = climbStairsFibonacci(stairs6, maxSteps6)
 const calculatedSolution6_recursive = climbStairsRecursive(stairs6, maxSteps6)
 const calculatedSolution6_recursiveMemoized = climbStairsRecursiveMemoized(stairs6, maxSteps6)
+const calculatedSolution6_fibonacci = climbStairsFibonacci(stairs6, maxSteps6)
 
-console.log(`Example Stairs #6: ${stairs6}, maxSteps: ${m6}, Solution: ${solution6}`)
-assert.deepStrictEqual(calculatedSolution6, solution6)
+console.log(`Example Stairs #6: ${stairs6}, maxSteps: ${maxSteps6}, Solution: ${solution6}`)
 assert.deepStrictEqual(calculatedSolution6_recursive, solution6)
 assert.deepStrictEqual(calculatedSolution6_recursiveMemoized, solution6)
+assert.deepStrictEqual(calculatedSolution6_fibonacci, solution6)
