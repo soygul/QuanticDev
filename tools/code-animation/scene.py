e1d46b14a08238acd042bcc348dcc90482b68ebc
Teoman Soygul <teo@soygul.com>
2020-12-07 01:07:29 +0100

    tools: fix code animation ranges

--- tools/code-animation/scene.py
+++ tools/code-animation/scene.py
@@ -1,14 +1,20 @@
 from manim import *
 
 
-def highlight_line(self, tex: Code, number: int = -1):
-    return [ApplyMethod(tex.code[line_no].set_opacity, .3 if line_no != number and number != -1 else 1) for line_no in range(len(tex.code))]
+def highlight_line(code: Code, line_from, line_to):
+    if not line_to:
+        line_to = line_from
+    lines = range(line_from, line_to)
+
+    return [ApplyMethod(code.code[line_no].set_opacity, 1 if line_no in lines else .3) for line_no in range(len(code.code))]
 
 
 class AnimatedCode(Scene):
     def construct(self):
-        tex = Code("code.py")
-        self.play(ShowCreation(tex, lag_ratio=5))
+        code = Code("code.py")
+        self.play(ShowCreation(code, lag_ratio=5))
         self.wait()
-        self.play(*self.highlight_line(tex, 4))
+        self.play(*highlight_line(code, 2, 6))
+        self.wait()
+        self.play(*highlight_line(code, 8, 10))
         self.wait()
