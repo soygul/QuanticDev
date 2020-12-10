7cbf16979e63d6f6c61e83d84b012954196fda91
Teoman Soygul <teo@soygul.com>
2020-12-10 14:17:13 +0100

    tools: code-animation: add transform template

--- tools/code-animation/main.py
+++ tools/code-animation/main.py
@@ -12,7 +12,9 @@ def highlight_line(code: Code, line_from, line_to):
 def create_code(code_src: str, ext: str = 'py'):
     file = f'code.{ext}'
     with open(file, 'w') as f:
-        f.write(code_src)
+        f.write(code_src.replace("""
+""", """ 
+"""))  # hack: not to get newlines trimmed by LaTex renderer, we replace '\n' with '\n '
     code = Code(file)
     with open(file, 'w') as f:
         f.write('')
@@ -26,22 +28,111 @@ class AnimatedCode(Scene):
 
     def animation_template(self):
         code = create_code("""xxxxxx""")
+        self.clear()
         self.play(ShowCreation(code, run_time=10))
         self.wait()
 
         self.play(*highlight_line(code, 2, 6))
         self.wait()
 
-        self.clear()
+        code2 = create_code("""xxxxxx""")
+        self.play(Transform(code, code2))
+        self.wait()
 
     def construct(self):
-        code1 = create_code("""db.connect('postgresql://localhost/testdb')
-db.initialize()""")
-        self.play(ShowCreation(code1, run_time=10))
+        code = create_code("""db.connect('postgresql://localhost/testdb')
+db.initialize()
+
+user = db.create_new_user('John Doe', 'john.doe@quanticdev.com')
+user.give_permission('create_blog_post')
+
+post = user.new_blog_post('Test Blog Post Title', 'Lorem ipsum dolor sit amet...')
+post.upload_thumbnail('./test_thumbnail.jpg')
+post.publish()""")
+        self.play(ShowCreation(code, run_time=10))
         self.wait()
 
-        self.clear()
-
-        code2 = create_code("""db.connect('postgresql://localhost/testdb').initialize()""")
-        self.play(ShowCreation(code2, run_time=10))
+        code2 = create_code("""db.connect('postgresql://localhost/testdb') \\
+    .initialize() \\
+    .create_new_user('John Doe', 'john.doe@quanticdev.com') \\
+    .give_permission('create_blog_post') \\
+    .new_blog_post('Test Blog Post Title', 'Lorem ipsum dolor sit amet...') \\
+    .upload_thumbnail('./test_thumbnail.jpg') \\
+    .publish()""")
+        self.play(Transform(code, code2))
         self.wait()
+
+#         self.clear()
+#
+#         code = create_code("""db.connect('postgresql://localhost/testdb')
+# db.initialize()""")
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
+#
+#         self.clear()
+#
+#         code = create_code("""db.connect('postgresql://localhost/testdb').initialize()""")
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
+#
+#         self.clear()
+#
+#         code = create_code("""db.connect('postgresql://localhost/testdb') \\
+#     .initialize() \\
+#     .close()""")
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
+#
+#         self.clear()
+#
+#         code = create_code("""class DB:
+#     def connect(self, url):
+#         pass
+#
+#     def initialize(self):
+#         pass
+#
+#     def close(self):
+#         pass""")
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
+#
+#         self.clear()
+#
+#         code = create_code("""class DB:
+#     def connect(self, url):
+#         # todo
+#         return self
+#
+#     def initialize(self):
+#         # todo
+#         return self
+#
+#     def close(self):
+#         # todo
+#         return selfs""")
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
+#
+#         self.clear()
+#
+#         code = create_code("""db.connect('postgresql://localhost/testdb')
+# log.info('Initializing the database.')
+# db.initialize()""")
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
+#
+#         self.clear()
+#
+#         code = create_code("""db.connect('postgresql://localhost/testdb') \\
+#     .log_info('Initializing the database.') \\
+#     .initialize()""")
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
+#
+#         code = create_code("""subscribers
+#   .filter(p => p.liked_the_video === true)
+#   .sort(p => p.name)
+#   .pat_on_the_back()""", ext='js')
+#         self.play(ShowCreation(code, run_time=10))
+#         self.wait()
