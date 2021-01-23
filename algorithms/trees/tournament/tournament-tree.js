3c06f2921b404a9e748cf7d43135b4e0be19e372
Teoman Soygul <teo@soygul.com>
2021-01-23 11:39:46 +0100

    tournament: snapshot

--- algorithms/trees/tournament/tournament-tree.js
+++ algorithms/trees/tournament/tournament-tree.js
@@ -1,1 +1,11 @@
-module.exports = class {}
+module.exports = class {
+  constructor (dataArr) {
+    this.leaves = dataArr
+
+    // we need to have 2^n leaves
+    let n = 1
+    while (this.leaves.length % 2 !== 0) {
+      n++
+    }
+  }
+}
