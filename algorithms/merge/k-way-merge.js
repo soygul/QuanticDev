5a25100ca72d4be71ab2c07eff32e29ed18187bc
Teoman Soygul <teo@soygul.com>
2021-01-26 00:04:01 +0100

    k-way merge: add implementation and test

--- algorithms/merge/k-way-merge.js
+++ algorithms/merge/k-way-merge.js
@@ -1,4 +1,5 @@
 const assert = require('assert')
+const TournamentTree = require('../trees/tournament/tournament-tree')
 
 /**
  * Given k sorted arrays, merge them into a single sorted array.
@@ -17,6 +18,23 @@ const assert = require('assert')
 function kWayMerge (inputArrays) {
   // validate the input
   assert(Array.isArray(inputArrays) && inputArrays.every(arr => Array.isArray(arr)), 'Input should be an array of arrays.')
+
+  const totalElements = inputArrays.map(a => a.length).reduce((total, current) => total + current)
+  const tree = new TournamentTree(inputArrays.map(a => a.shift()))
+  const mergedArr = []
+
+  // put the top element from each array into a tournament tree and get the smallest element from the tree root
+  for (let i = 1; i <= totalElements; i++) {
+    mergedArr.push(tree.popRoot())
+
+    if (inputArrays[tree.missingLeafIndex]) {
+      tree.pushLeaf(inputArrays[tree.missingLeafIndex].shift())
+    } else {
+      tree.pushLeaf(Infinity)
+    }
+  }
+
+  return mergedArr
 }
 
 /**
@@ -24,61 +42,10 @@ function kWayMerge (inputArrays) {
  */
 
 // test case #1: mixed integers
-const exampleInput1 = [-5, 2, -1, 3, -2]
-const maxSolution1 = [2, -1, 3]
-const minSolution1 = [-5]
+const exampleInput1 = [[4, 9], [1, 7], [3, 6]]
+const solution1 = [1, 3, 4, 6, 7, 9]
 
-const calculatedMaxSolution1 = getMaxMinSubarray(exampleInput1, true)
-const calculatedMinSolution1 = getMaxMinSubarray(exampleInput1, false)
+const calculatedSolution1 = kWayMerge(exampleInput1)
 
-console.log(`Example Input #1: ${JSON.stringify(exampleInput1)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution1)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution1)}`)
-assert.deepStrictEqual(calculatedMaxSolution1, maxSolution1)
-assert.deepStrictEqual(calculatedMinSolution1, minSolution1)
-
-// test case #2: empty array
-const exampleInput2 = []
-const maxSolution2 = []
-const minSolution2 = []
-
-const calculatedMaxSolution2 = getMaxMinSubarray(exampleInput2, true)
-const calculatedMinSolution2 = getMaxMinSubarray(exampleInput2, false)
-
-console.log(`Example Input #2: ${JSON.stringify(exampleInput2)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution2)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution2)}`)
-assert.deepStrictEqual(calculatedMaxSolution2, maxSolution2)
-assert.deepStrictEqual(calculatedMinSolution2, minSolution2)
-
-// test case #3: all 0's
-const exampleInput3 = [0, 0, 0]
-const maxSolution3 = []
-const minSolution3 = []
-
-const calculatedMaxSolution3 = getMaxMinSubarray(exampleInput3, true)
-const calculatedMinSolution3 = getMaxMinSubarray(exampleInput3, false)
-
-console.log(`Example Input #3: ${JSON.stringify(exampleInput3)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution3)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution3)}`)
-assert.deepStrictEqual(calculatedMaxSolution3, maxSolution3)
-assert.deepStrictEqual(calculatedMinSolution3, minSolution3)
-
-// test case #4: all negative
-const exampleInput4 = [-1, -2, -3, -4, -5]
-const maxSolution4 = []
-const minSolution4 = [-1, -2, -3, -4, -5]
-
-const calculatedMaxSolution4 = getMaxMinSubarray(exampleInput4, true)
-const calculatedMinSolution4 = getMaxMinSubarray(exampleInput4, false)
-
-console.log(`Example Input #4: ${JSON.stringify(exampleInput4)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution4)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution4)}`)
-assert.deepStrictEqual(calculatedMaxSolution4, maxSolution4)
-assert.deepStrictEqual(calculatedMinSolution4, minSolution4)
-
-// test case #5: all positive
-const exampleInput5 = [1, 2, 3, 4, 5]
-const maxSolution5 = [1, 2, 3, 4, 5]
-const minSolution5 = []
-
-const calculatedMaxSolution5 = getMaxMinSubarray(exampleInput5, true)
-const calculatedMinSolution5 = getMaxMinSubarray(exampleInput5, false)
-
-console.log(`Example Input #5: ${JSON.stringify(exampleInput5)}, Max Sum Subarray: ${JSON.stringify(calculatedMaxSolution5)}, Min Sum Subarray: ${JSON.stringify(calculatedMinSolution5)}`)
-assert.deepStrictEqual(calculatedMaxSolution5, maxSolution5)
-assert.deepStrictEqual(calculatedMinSolution5, minSolution5)
+console.log(`Example Input #1: Expected Solution: ${JSON.stringify(solution1)}, Calculated Solution: ${JSON.stringify(calculatedSolution1)}`)
+assert.deepStrictEqual(calculatedSolution1, solution1)
