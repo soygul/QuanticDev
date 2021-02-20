f06131884b7efeb975490058bd2bc46f1d1d9967
Teoman Soygul <teo@soygul.com>
2021-02-20 20:46:25 +0100

    insertion-sort: add tests

--- algorithms/sort/insertion-sort.js
+++ algorithms/sort/insertion-sort.js
@@ -22,16 +22,17 @@ function insertionSort (data) {
   for (let i = 1, current = data[i]; i < data.length; i++, current = data[i]) {
     // shift elements up until we find the correct spot for the current element
     for (let j = i - 1; j >= 0; j--) {
-      if (data[j] > current) {
-        data[j + 1] = data[j]
-      } else {
-        data[j] = current
+      if (data[j] <= current) {
+        data[j + 1] = current
         break
+      } else  {
+        data[j + 1] = data[j]
+        if (j === 0) data[0] = current
       }
     }
   }
 
-  return []
+  return data
 }
 
 /**
@@ -39,10 +40,19 @@ function insertionSort (data) {
  */
 
 // test case #1
-const exampleInput1 = [[4, 9], [1, 7], [3, 6]]
-const solution1 = [1, 3, 4, 6, 7, 9]
+const exampleInput1 = [2, 3, 4, 1, 5]
+const solution1 = [1, 2, 3, 4, 5]
 
 const calculatedSolution1 = insertionSort(exampleInput1)
 
 console.log(`Example Input #1: Expected Solution: ${JSON.stringify(solution1)}, Calculated Solution: ${JSON.stringify(calculatedSolution1)}`)
 assert.deepStrictEqual(calculatedSolution1, solution1)
+
+// test case #1
+const exampleInput2 = [5, 0, -2, 1, 0, 5, 1]
+const solution2 = [-2, 0, 0, 1, 1, 5, 5]
+
+const calculatedSolution2 = insertionSort(exampleInput2)
+
+console.log(`Example Input #2: Expected Solution: ${JSON.stringify(solution2)}, Calculated Solution: ${JSON.stringify(calculatedSolution2)}`)
+assert.deepStrictEqual(calculatedSolution2, solution2)
